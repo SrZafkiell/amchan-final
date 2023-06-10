7f2178d6e8631d713a79bb55b0a1eb7823b1adc9
SrZafkiell <daniel.rayo@correounivalle.edu.co>
2023-06-10 18:43:21 -0500

    ControlPanel update: Little design and starting to use the fetched data to test

--- src/assets/components/Pages/ControlPanel/ControlPanel.jsx
+++ src/assets/components/Pages/ControlPanel/ControlPanel.jsx
@@ -3,10 +3,13 @@ import { StoreContext } from "../../../context/StoreContext";
 import { Loader } from "../../Loader/Loader";
 
 import styles from "./ControlPanel.module.css";
+import DefaultContent from "./Elements/DefaultContent/DefaultContent";
 
 export const ControlPanel = () => {
-  const { allCarts, loadingCarts } = useContext(StoreContext);
+  const { allCarts, loadingCarts, allProducts, loadingProducts } = useContext(StoreContext);
   const [showControlPanelDefault, setShowControlPanelDefault] = useState(true);
+  const [showAccountManagement, setShowAccountManagement] = useState(false);
+  const [showProductManagement, setShowProductManagement] = useState(false);
   const [showCarts, setShowCarts] = useState(false);
   const [showTest, setShowTest] = useState(false);
   const [loadingFinished, setLoadingFinished] = useState(false);
@@ -15,35 +18,59 @@ export const ControlPanel = () => {
     setShowCarts(true);
     setShowControlPanelDefault(false);
     setShowTest(false);
+    setShowAccountManagement(false);
+    setShowProductManagement(false);
   };
 
   const handleShowTest = () => {
     setShowTest(true);
     setShowControlPanelDefault(false);
     setShowCarts(false);
+    setShowAccountManagement(false);
+    setShowProductManagement(false);
   };
 
+  const handleShowAccountManagement = () => {
+    setShowTest(false);
+    setShowControlPanelDefault(false);
+    setShowCarts(false);
+    setShowAccountManagement(true);
+    setShowProductManagement(false);
+  }
+
+  const handleShowProductsManagement = () => {
+    setShowTest(false);
+    setShowControlPanelDefault(false);
+    setShowCarts(false);
+    setShowAccountManagement(false);
+    setShowProductManagement(true);
+  }
+
   useEffect(() => {
-    if (!loadingCarts) {
+    if (!loadingCarts && !loadingProducts) {
       // Loading is finished, show default content
       setLoadingFinished(true);
     }
-  }, [loadingCarts]);
+  }, [loadingCarts, loadingProducts]);
 
   return (
     <>
       <div className={styles.controlPanelMain}>
         <div className={styles.controlPanelMenu}>
-          <button onClick={handleShowCarts}>Show Carts</button>
-          <button onClick={handleShowTest}>Show Test</button>
+          <button className={styles.controlPanelSubButton} onClick={handleShowCarts}>Sales Analytics</button>
+          <button className={styles.controlPanelSubButton} onClick={handleShowTest}>Most buyed</button>
+          <button className={styles.controlPanelSubButton} onClick={handleShowTest}>Test</button>
+          <button className={styles.controlPanelSubButton} onClick={handleShowAccountManagement}>Edit Accounts</button>
+          <button className={styles.controlPanelSubButton} onClick={handleShowProductsManagement}>Edit Products</button>
+          {/* <button className={styles.controlPanelSubButton} onClick={handleShowTest}></button> */}
         </div>
         <div className={styles.controlPanelContent}>
-          <div className={styles.loaderWrapper} style={{ display: loadingCarts ? "block" : "none" }}>
+          <div className={styles.loaderWrapper} style={{ display: loadingCarts || loadingProducts ? "block" : "none" }}>
             <Loader />
           </div>
           {loadingFinished && showControlPanelDefault && (
             <>
-              <h1>Default content</h1>
+              <DefaultContent />
             </>
           )}
           {showCarts && (
@@ -66,7 +93,32 @@ export const ControlPanel = () => {
           )}
           {showTest && (
             <>
-              <h1>Test</h1>
+              <div className={styles.productsContainer} style={{ display: loadingProducts ? "none" : "block" }}>
+                {allProducts.map((product) => (
+                  <div key={product.id}>
+                    <h3>Product ID: {product.id}</h3>
+                    <ul>
+                      {Object.keys(product).map((key) => (
+                        <li key={key}>
+                          {key}: {JSON.stringify(product[key])}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+          {showAccountManagement && (
+            <>
+              <h1>Account Management</h1>
+              <h2>Under construction</h2>
+            </>
+          )}
+          {showProductManagement && (
+            <>
+              <h1>Products Management</h1>
+              <h2>Under construction</h2>
             </>
           )}
         </div>
---
+++ src/assets/components/Pages/ControlPanel/Elements/DefaultContent/DefaultContent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import fonts from '/src/assets/fonts/GlobalFonts.module.css'
+import styles from './DefaultContent.module.css'
+
+function DefaultContent() {
+
+    return (
+        <div className={styles.defaultContentBox}>
+            <article>
+                <h1 className={fonts.titleFont}>¡Welcome USER to the Control Panel!</h1>
+                <section id="whatisthis">
+                    <h2 className={fonts.subTitleFont}>What is this?</h2>
+                    <p className={fonts.textFont}>To the left of this section, you will see some options to manage products and analitics.</p>
+                </section>
+                <section id="statics">
+                    <h2 className={fonts.subTitleFont}>Analitics?</h2>
+                    <p className={fonts.textFont}>Yes! We provide some metrics about the products and various options of visualizing.</p>
+                </section>
+                <section id="updates">
+                    <h2 className={fonts.subTitleFont}>Updates?</h2>
+                    <p className={fonts.textFont}><a className={fonts.linkFont} href="https://github.com/SrZafkiell">SrZafkiell</a> will be offering updates for this project till he considers it is finished.</p>
+                    <p className={fonts.textFont}>The implemented updates and upcoming ones can be found in this: <a className={fonts.linkFont} href="https://trello.com/es">Updates list</a></p> 
+                </section>
+                {/* <section>
+                    <h2 className={fonts.subTitleFont}></h2>
+                    <p className={fonts.textFont}></p>
+                </section> */}
+            </article>
+        </div>
+    )
+};
+
+export default DefaultContent;
